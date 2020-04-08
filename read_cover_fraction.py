63365a1d8407a9b4cf48de10d6bec4fa9d808124
gbessardon <geoffreybess@DESKTOP-LVSG0E9.localdomain>
2020-04-08 09:09:40 +0100

    starting new git

---
+++ read_cover_fraction.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 12:45:02 2019
+
+@author: gbessardon
+"""
+import numpy as np
+
+#reads the cover fraction data from ECOCLIMAP_II and ECOCLIMAP_SG
+#filename='/home/gbessardon/ECOCLIMAP/ECOCLIMAP_II_cover_data.csv'
+
+def cover_fraction(filename):
+    data=open(filename)
+    covernum=[]
+    covername=[]
+    cityfrac=[]
+    vegfrac=[]
+    inwaterfrac=[]
+    seafrac=[]
+    for i,d in enumerate(data):
+        if i==0:
+            fieldnames=d.split(',')
+        else:
+#            print(d.split(','))
+            covernum.append(int(d.split(',')[0]))
+            covername.append(d.split(',')[1])
+            cityfrac.append(float(d.split(',')[2]))
+            vegfrac.append(float(d.split(',')[3]))
+            inwaterfrac.append(float(d.split(',')[4]))
+            seafrac.append(float(d.split(',')[5]))
+    return(fieldnames,np.array(covernum),covername,np.array(cityfrac),np.array(vegfrac),
+           np.array(inwaterfrac),np.array(seafrac))
